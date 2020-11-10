91b042758d30f28a12ba37310a8d7aa21dd0c198
雪浩 <haoguchen1@gmail.com>
2020-11-10 16:29:42 +0800

    Revert "20201107"
    This reverts commit 4f6de0182eeaef1d8a257f86988ecd9473e53943.

--- tp.edxmpp.org/application/controllers/User.php
+++
@@ -1,9 +0,0 @@
-<?php
-class User extends CI_Controller
-{
-    public function index($user_id = null)
-    {
-        // We load the CI welcome page with some lines of Javascript
-        $this->load->view('welcome_message', array('user_id' => $user_id));
-    }
-}
